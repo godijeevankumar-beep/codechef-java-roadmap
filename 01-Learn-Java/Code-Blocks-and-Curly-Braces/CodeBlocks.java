8cae8b3bd33523a6af3fa477c2e15660d8ff8abe
godijeevankumar-beep <godijeevankumar@gmail.com>
2026-07-08 19:32:00 +0530

    Add CodeBlocks.java to demonstrate code blocks

---
+++ 01-Learn-Java/Code-Blocks-and-Curly-Braces/CodeBlocks.java
@@ -0,0 +1,14 @@
+class Codechef {
+    public static void main(String[] args) {
+
+        // Execute the first block
+        {
+            System.out.println("Executing Block 1");
+        }
+
+        // Execute the second block
+        {
+            System.out.println("Executing Block 2");
+        }
+    }
+}
